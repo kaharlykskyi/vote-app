6b2467a9b686fe69504504e195467c5ff70b8b81
Misha Kaharlykskyi <mishakagar@gmail.com>
2024-06-11 18:35:35 +0200

    adjust breeze edit-profile components to design

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-6 py-3 bg-blue border border-blue rounded-xl font-semibold text-xs text-white hover:bg-blue-hover focus:bg-blue active:bg-blue focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 h-11']) }}>
     {{ $slot }}
 </button>
